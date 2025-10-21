939c2dd521ab2352c45e8cf4921bdcf0538b5559
Andrei8288 <andreigspircu@gmail.com>
2025-10-21 17:59:55 +0200

    uncomment test_options()

--- Scripts/manager.gd
+++ Scripts/manager.gd
@@ -8,11 +8,10 @@ extends Node2D
 
 func _ready() -> void:
 	spawn_npc()
-	#test_options()
+	test_options()
 
 
 func spawn_npc():
-	randomize()
 	for i in npc_count:
 		var npc = npc_scene.instantiate()
 		var x = randf_range(-spawn_area.x/2,spawn_area.y/2)
@@ -25,10 +24,10 @@ func test_options():
 	for npc in npc_container.get_children():
 		var option = npc.get_node("OptionTextOutput")
 		option.duration = 2.0
-		option.cooldown = 0.0
+		option.cooldown = 3.0
 		option.text = "Testing " + npc.name
 		call_deferred("start_test", option)
-		
+
 func start_test(option):
 	option.start()
 	option.pause()
